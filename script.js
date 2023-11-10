6ba7aaf135ce1124955fdceee518dfd12fef259a
GSinghh <gbassans@yahoo.com>
2023-11-09 17:35:52 -0800

    Created file for api key

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('search-bar')
+
+const weatherData = async () => {
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}`
+}
+
+/* http://api.weatherapi.com/v1/current.json?key=11bd83245017426cbcd215405230811&q=New York&aqi=no
+ */
