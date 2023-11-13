5e94bf47ae276f8bad6311a49b4a48a6fbf41697
GSinghh <gbassans@yahoo.com>
2023-11-12 23:20:24 -0800

    Split up API functions

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
-document.getElementById('search-bar')
+import API_KEY from "./api_key.mjs"
 
-const weatherData = async () => {
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}`
+const getDataFromForm = () => {
+    const searchQuery = document.getElementById('search-bar');
+    if(searchQuery)
+    {
+        return searchQuery.value;
+    }
+    else
+    {
+        return console.error("Invalid City Name")
+    }
 }
 
-/* http://api.weatherapi.com/v1/current.json?key=11bd83245017426cbcd215405230811&q=New York&aqi=no
- */
+const getWeatherData = async (CITY_NAME) => {
+    try {
+        const response = await fetch(urlBuilder(CITY_NAME));
+        const weatherData = await response.json();
+        return weatherData;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+const urlBuilder = (CITY_NAME) => {
+    return `https://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}`
+}
+
+// const city = getDataFromForm();
+// console.log(city)
+let obj = await getWeatherData("San Jose");
+console.log(obj)
+
+
+
+// const parseArray = () => {
+
+//     const weatherData = [
+//   // ... (your 40 weather objects)
+// ];
+
+// // Function to split array into chunks
+// function chunkArray(arr, chunkSize) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i += chunkSize) {
+//     result.push(arr.slice(i, i + chunkSize));
+//   }
+//   return result;
+// }
+
+// // Split the weather data into chunks representing one day
+// const dailyWeatherChunks = chunkArray(weatherData, 8);
+
+// console.log(dailyWeatherChunks);
+
+
+// }
