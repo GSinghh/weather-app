7956ab9fe22eeccc77a56d15d7de209fafa6caee
GSinghh <gbassans@yahoo.com>
2023-12-01 23:54:59 -0800

    Finished render data function

--- js/domMethods.js
+++ js/domMethods.js
@@ -4,15 +4,49 @@ const allWeatherConditions = {
     'Snow': 'images/SVG/snow.svg',
     'Rain': 'images/SVG/rainy.svg',
     'Thunderstorm': 'images/SVG/lighting.svg',
+};
 
-
+const renderData = (avgTemps, weatherPerDay) => {
+    const weatherContainer = document.querySelector('.weather-container');
+    weatherContainer.innerHTML = ' ';
+    for (const index in avgTemps)
+    {
+        let temp = avgTemps[index];
+        let weatherCondition = weatherPerDay[index];
+        // console.log(index, temp, allWeatherConditions[weatherCondition]);
+        let newDiv = createDiv(allWeatherConditions[weatherCondition], temp, index)
+        weatherContainer.appendChild(newDiv);
+    }
 }
 
-const renderData = () => {
+const createDiv = (path, temp, date) => {
+    
+    const newDiv = document.createElement('div');
+    newDiv.setAttribute('class','weather-icon');
+
+    //Creating Image
+    const image = document.createElement('img');
+    image.src = path;
+
+    const weatherTemp = document.createElement('div');
+    weatherTemp.setAttribute('class','weather-temp');
+    weatherTemp.textContent = temp;
+
+    const weatherDate = document.createElement('div');
+    weatherDate.setAttribute('class', 'weather-date');
+    weatherDate.textContent = date;
+
+    newDiv.appendChild(image);
+    newDiv.appendChild(weatherTemp);
+    newDiv.appendChild(weatherDate);
+
+    return newDiv;
 
 }
 
 const changeCity = (city) => {
-    const cityName = document.querySelector('city-name');
+    const cityName = document.querySelector('.city-name');
     cityName.value = city;
 }
+
+export {changeCity, renderData}
